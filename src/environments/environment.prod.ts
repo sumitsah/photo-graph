b12c7fcebb21eeaf7d8ee77fb9daad13841a523c
sumit.sahu <sumit.sahu@clariontechnologies.co.in>
2020-10-30 11:09:30 +0530

    Generated deployables for github pages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBu0tIB67N1OeX8uboTozsbkBpoeWYlfoA",
+    authDomain: "image-gellery.firebaseapp.com",
+    databaseURL: "https://image-gellery.firebaseio.com",
+    projectId: "image-gellery",
+    storageBucket: "image-gellery.appspot.com",
+    messagingSenderId: "18814594923",
+    appId: "1:18814594923:web:adef6803b43c804714662e",
+    measurementId: "G-07KZY37N58"
+  }
 };
